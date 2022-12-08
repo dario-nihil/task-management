8eb05acfde001748f7bf84ae1088a2dea6194499
dario-nihil <dario.nihil@gmail.com>
2022-12-08 09:02:48 +0100

    implemented creation of task

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,12 +1,11 @@
 export interface Task {
   id: string;
   title;
-  string;
   description: string;
   status: TaskStatus;
 }
 
-enum TaskStatus {
+export enum TaskStatus {
   OPEN = 'OPEN',
   IN_PRORGESS = 'IN_PROGRESS',
   DONE = 'DONE',
