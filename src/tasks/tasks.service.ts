f6f1aaabcd8f2f4c4d70589c719f5a8a146614aa
dario-nihil <dario.nihil@gmail.com>
2022-12-08 14:01:34 +0100

    implemented throwing exception in creating or updating a task that not exists

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -15,7 +15,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) throw new NotFoundException(`Task with ID "${id}" not found`);
+
+    return found;
   }
 
   getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
