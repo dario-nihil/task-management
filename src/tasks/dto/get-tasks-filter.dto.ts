4408ccf8bd23c346866df2f98310e11e2a0a2ea3
dario-nihil <dario.nihil@gmail.com>
2022-12-08 14:32:06 +0100

    implemented validation for searching and filtering on tasks

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
 import { TaskStatus } from '../task.model';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { isString } from 'util';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
