ecf9aee87c2144c1c6923749968e172c4bc659cf
dario-nihil <dario.nihil@gmail.com>
2022-12-12 08:58:13 +0100

    created authCredential dto, implemented logic for signUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
 
 @Injectable()
@@ -7,4 +8,8 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private usersRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
